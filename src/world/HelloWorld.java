c190f41a99013da44a065d63b15f9b56fe6e5788
brian <brianeazer2015@gmail.com>
2018-10-09 11:50:44 -0400

    Second commit with comments

--- src/world/HelloWorld.java
+++ src/world/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// This is a program that prints Hello World to the console
 		System.out.println("Hello world!");
 	}
 
